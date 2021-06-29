dfb2332c06ca7f97977925ffaed40f405c525612
aracelly <aracellybootcamp@gmail.com>
2021-06-29 19:10:54 -0400

    avance en js no terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,25 @@
 $(function () {
   // Handler for .ready() called.
   const xlarge = 1199.98;
+  const medium = 767.98;
   window.addEventListener("load", function () {
-    if ($(window).width() >= xlarge) {
-      let hMain = $('main').height();
-      let hHeader = $('header').height();
-
-      if (hMain > hHeader)
-        $('header').height(hMain);
-      else
-        $('main').height(hHeader);
-    }
+    if ($(window).width() >= xlarge)
+      $('header').height($('main').height());
+    console.log("load: header " + $('header').height()+ " main " +  $('main').height());
   });
 
   $(window).resize(function () {
-    if ($(window).width() >= xlarge) {
-      let hMain = $('main').height();
-      let hHeader = $('header').height();
-      if (hMain > hHeader)
-        $('header').height(hMain);
+    console.log("resize");
+    if ($(window).width() >= xlarge)
+      $('header').height($('main').height());
+    else {
+      if ($(window).width() >= medium)
+        $('header').height(0.44 * $(window).width());
       else
-        $('main').height(hHeader);
+        $('header').height(0.58 * $(window).width());
     }
-});
+    console.log("windows.width " + $(window).width());
+    console.log("main " + $('main').height());
+    console.log("header " + $('header').height());
+  });
 });

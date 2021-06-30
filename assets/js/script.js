a0361702c41411d3399d25e59dc3338d189be44c
aracelly <aracellybootcamp@gmail.com>
2021-06-29 20:28:57 -0400

    js finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,16 @@ $(function () {
   // Handler for .ready() called.
   const xlarge = 1199.98;
   const medium = 767.98;
-  window.addEventListener("load", function () {
-    if ($(window).width() >= xlarge)
-      $('header').height($('main').height());
-    console.log("load: header " + $('header').height()+ " main " +  $('main').height());
-  });
+
+  if ($(window).width() >= xlarge)
+    $('header').height($('main').height());
+  else {
+    if ($(window).width() >= medium)
+      $('header').height(0.44 * $(window).width());
+    else
+      $('header').height(0.58 * $(window).width());
+  }
+  console.log("load: header " + $('header').height() + " main " + $('main').height());
 
   $(window).resize(function () {
     console.log("resize");
